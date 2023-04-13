82dce612b109f7b361fe9aac768b6092f6f059de
loxt <emannuelloxt@hotmail.com>
2023-04-12 22:16:00 -0400

    feat: add validation pipe

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { ReservationsModule } from './reservations.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
+
+  await app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(8080);
 }
 bootstrap();
