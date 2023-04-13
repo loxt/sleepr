3f80bc3b1ac1fd05ed2526797c5e4b0b67e85af3
loxt <emannuelloxt@hotmail.com>
2023-04-12 20:24:21 -0400

    feat: database and config module

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@app/common/config';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/sleepr')],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class DatabaseModule {}
